b4dbb44aa6057aa4f1ffa2356947e556c0902122
drsunshine80-coder <dr.sunshine80@gmail.com>
2026-05-20 22:17:49 -0600

    File-Based Routing - Link Components

---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const initialTickets = [
+  {
+    id: "1",
+    title: "Ticket 1",
+    content: "Description for Ticket 1",
+    status: "DONE",
+  },
+  {
+    id: "2",
+    title: "Ticket 2",
+    content: "Description for Ticket 2",
+    status: "DONE",
+  },
+];
